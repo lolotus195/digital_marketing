d5b7cccdb42daf1c6b4b26535feb9e4d52923c31
Matthew DeLio <mdelio@gmail.com>
2016-04-25 15:34:08 -0500

    Add exploration plots.

---
+++ hw3/exploration.R
@@ -0,0 +1,44 @@
+####
+# Setup ----
+####
+rm(list=ls())
+source("../utils/source_me.R", chdir = T)
+CreateDefaultPlotOpts()
+
+require(plyr)
+
+load("hw3.rdat")
+# Gender should be a factor variable!
+RelevelGender <- function(x) {
+  x <- factor(x)
+  x <- revalue(x, c('1'='female', '2'='male'))
+  return(x)
+}
+df$SenderGender <- RelevelGender(df$SenderGender)
+df$ReceiverGender <- RelevelGender(df$ReceiverGender)
+
+
+####
+# Heatmap of Data by Gender ----
+####
+g <- ggplot(data=df, aes(x=SenderLooks, y=ReceiverLooks)) + 
+  stat_bin2d(aes(fill=..count..), drop=T) +
+  scale_fill_gradient("Sample\nCount") +
+  facet_wrap("SenderGender", labeller =labeller(
+    SenderGender=c(female="Female Sender", male="Male Sender"))) +
+  coord_equal() + theme_bw() + 
+  labs(x="Sender Looks", y="Receiver Looks")
+GGPlotSave(g, "q0_data_heat")
+
+
+####
+# Heatmap of Contacts by Gender ----
+####
+ggplot(data=df[df$y==1,], aes(x=SenderLooks, y=ReceiverLooks)) + 
+  stat_bin2d(aes(fill=..count..), drop=T) +
+  scale_fill_gradient("Sample\nCount") +
+  facet_wrap("SenderGender", labeller =labeller(
+    SenderGender=c(female="Female Sender", male="Male Sender"))) +
+  coord_equal() + theme_bw() + 
+  labs(x="Sender Looks", y="Receiver Looks")
+GGPlotSave(g, "q0_contact_heat")

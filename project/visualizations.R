c4c5e2f996b8e633d30e112416b903413c74c4b9
Will Clark <wclark3@gmail.com>
2016-05-10 16:38:39 -0500

    tweak some vizis

--- project/visualizations.R
+++ project/visualizations.R
@@ -27,12 +27,14 @@ g <- g +
                    xend = log(data$lambda[length(data$lambda)*0.8]), 
                    y = 0.2495, yend = 0.2495),
                size = 0.3, color = '#000000', 
-               arrow = arrow(angle = 30, length = unit(0.2, 'inches'), ends = 'first')) + 
+               arrow = arrow(angle = 20, length = unit(0.2, 'inches'), 
+                             type = 'closed', ends = 'first')) + 
   geom_segment(aes(x = log(data$lambda[length(data$lambda)*0.2]), 
                    xend = max(log(data$lambda)), 
                    y = 0.2495, yend = 0.2495),
                size = 0.3, color = '#000000', 
-               arrow = arrow(angle = 30, length = unit(0.2, 'inches'), ends = 'last')) +
+               arrow = arrow(angle = 20, length = unit(0.2, 'inches'), 
+                             type = 'closed', ends = 'last')) +
   annotate('text', x = min(log(data$lambda)), y = 0.25,
            hjust = 0, vjust = 0,
            label = 'more complex', size = 5) + 
@@ -42,23 +44,29 @@ g <- g +
 
 # Labels and formatting
 g <- g + 
-  labs(x = 'log(Lambda)', y = 'Binomial Deviance') +
+  labs(x = 'log(Lambda)', y = 'Binomial Deviance (Out-of-Sample Accuracy)') +
   expand_limits(y = 0.249) +
-  theme_bw()
+  theme_bw() + 
+  theme(axis.title.x = element_text(size = 15, margin = margin(10,0,0,0)),
+        axis.text.x = element_text(size = 12),
+        axis.title.y = element_text(size = 15, margin = margin(0,15,0,0)),
+        axis.text.y = element_text(size = 12, angle = 90, hjust = 0.5))
 # plot(g)
 
 # Vertical lines to mark lambda.min, lambda.1se (initial plot)
 g1 <- g +  
-  geom_vline(xintercept = log(data$lambda.min), linetype = 'dashed') + 
-  geom_vline(xintercept = log(data$lambda.1se), linetype = 'dashed') + 
+  geom_vline(xintercept = log(data$lambda.min), linetype = 'dashed',
+             color = '#000080', size = 1) + 
+  geom_vline(xintercept = log(data$lambda.1se), linetype = 'dashed', color = '#9e9e9e') + 
   annotate('text', x = log(data$lambda.min), y = 0.261,
            hjust = 1, vjust = 1, size = 5,
            label = sprintf('lambda.min  \n%.d params  ', 
-                           sum(coef(data,'lambda.min')!=0))) +
+                           sum(coef(data,'lambda.min')!=0)),
+           color = '#000080', fontface = 'bold') +
   annotate('text', x = log(data$lambda.1se), y = 0.261,
            hjust = 0, vjust = 1, size = 5,
            label = sprintf('  lambda.1se\n  %.d params', 
-                           sum(coef(data,'lambda.1se')!=0)))
+                           sum(coef(data,'lambda.1se')!=0)), color = '#9e9e9e')
 
 # Vertical lines to mark lambda.min, lambda.1se (secondary plot)
 g2 <- g +  
@@ -74,3 +82,6 @@ g2 <- g +
            label = sprintf('  lambda.1se\n  %.d params', 
                            sum(coef(data,'lambda.1se')!=0)), 
            color = '#000080', fontface = 'bold')
+
+ggsave('slides/regpath1.pdf', g1)
+ggsave('slides/regpath2.pdf', g2)

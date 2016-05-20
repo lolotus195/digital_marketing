e8228d0e3b00c3bccfa5ac7d0e0f98b85b684cc7
Will Clark <wclark3@gmail.com>
2016-05-20 10:23:08 -0500

    some plots for the slides

--- project/visualizations.R
+++ project/visualizations.R
@@ -2,6 +2,7 @@ rm(list=ls())
 
 require(ggplot2)
 require(glmnet)
+require(stringr)
 
 source("../utils/source_me.R", chdir = T)
 CreateDefaultPlotOpts()
@@ -85,3 +86,56 @@ g2 <- g +
 
 ggsave('slides/regpath1.pdf', g1)
 ggsave('slides/regpath2.pdf', g2)
+
+# Predictive coefficients from cv.gamlr run ----
+
+load("../hw4/cached/q4_glmnet_cv_it2.dat")
+mdl <- data
+rm(data)
+
+mdl.coefs <- drop(coef(mdl))[-1]
+mdl.coefs <- mdl.coefs[mdl.coefs!=0]
+
+pdat <- data.frame(coef=mdl.coefs,
+                   name=names(mdl.coefs))
+pdat$cat <- sapply(pdat$name, function(n) {
+  if(grepl(':',n)) { # interaction  
+    v1 <- str_split(n,':')[[1]][1]
+    v2 <- str_split(n,':')[[1]][2]
+    return(paste(substr(v1,1,2), ':', substr(v2,1,2), sep=''))
+  }
+  else(return(substr(n,1,2)))
+})
+
+# Base plot
+g.base <- ggplot(pdat) + geom_bar(aes(y=coef,x=name,fill=cat), stat = 'identity') +
+  labs(x = 'Variable Name', y = 'Coefficient') +
+  scale_fill_discrete('Variable') +
+  theme_bw() + theme(axis.text.x=element_text(angle=90))
+plot(g.base)
+
+# Annotate interaction terms
+g.int <- g.base + 
+  annotate('rect', xmin = 1.5, xmax = 3.5, 
+           ymin = min(pdat$coef[(nrow(pdat)-1):nrow(pdat)]) - 0.01, ymax = 0.01, 
+           alpha = 0, color = '#000000') +
+  annotate('text', x = 2.5, y = min(pdat$coef[(nrow(pdat)-1):nrow(pdat)]) - 0.03,
+           hjust = 0.5, vjust = 1, label = 'Only 2 interaction\nterms (V1 and V2)')
+plot(g.int)
+
+# Annotate missing variable 3 and almost missing variable 6
+g.var3 <- g.base + 
+  geom_segment(aes(x = 4.5, xend = 4.5,
+                   y = 0, yend = -0.2),
+               arrow = arrow(ends='first')) + 
+  geom_segment(aes(x = 8, xend = 8,
+                   y = 0, yend = -0.2),
+               arrow = arrow(ends='first')) + 
+  annotate('text', x = 6.1, y = -0.22, 
+           hjust = 0.5, vjust = 1, 
+           label = 'Variables 3 and 6 have\nlittle to no predictive power')
+plot(g.var3)
+
+ggsave('slides/coefs_base.pdf', g.base)
+ggsave('slides/coefs_int.pdf', g.int)
+ggsave('slides/coefs_missing.pdf', g.var3)
